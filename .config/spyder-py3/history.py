9e855337c1d238c130df97b6edf2d67a40f2a1b1
Quang <quangnb2000@gmail.com>
2020-10-19 06:12:43 +0700

    Everything

---
+++ .config/spyder-py3/history.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Oct 18 19:03:00 2020)---
+import sys
+sys.executable
+
+## ---(Sun Oct 18 19:06:13 2020)---
+import sys
+sys.executable
+
+## ---(Sun Oct 18 19:07:19 2020)---
+import keras
+import tensorflow
+clear
+runcell(0, '/home/quang/.config/spyder-py3/temp.py')
+
+## ---(Sun Oct 18 19:11:11 2020)---
+import tensorflow
